6b67b16a4ac87e9a5f6c1e4209bf2ecada8f50fc
Jacque0 <jacqueline.cmessias@gmail.com>
2023-03-15 16:32:34 -0300

    :white_check_mark: adiciona primeiro teste e2e #44-46

---
+++ cypress/e2e/Store.cy.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-undef */
+context('Store', () => {
+  it('should display the store', () => {
+    cy.visit('http://localhost:3000/');
+    cy.get('body').contains('Brand');
+    cy.get('body').contains('Wrist Watch');
+  });
+});
